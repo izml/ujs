17505b0506db3eba8bfb7f2a00f07dd9b15d939d
izml <izml@qq.com>
2012-12-02 11:12:23 +0800

    [0.1.4.1]修复了一个小问题

--- InputCtrl.js
+++ InputCtrl.js
@@ -2,7 +2,7 @@
 // @name		InputBox Controller
 // @author		izml
 // @description	Add Control Buttons to the InputBox Which Likes IE 10: Clear data & Show password!
-// @version		0.1.4
+// @version		0.1.4.1
 // @created		2012-12-1
 // @lastUpdated	2012-12-2
 // @namespace	https://github.com/izml/
@@ -19,24 +19,25 @@ function InputCtrl(){
 	try{if(document.doctype.name=='wml')return;}catch(evt){};
 	var ins=document.getElementsByTagName('input');
 	for(var i=ins.length-1;i>=0;i--){
-		if(ins[i].disabled) continue;
+		var input=ins[i];
+		if(input.disabled) continue;
 		try{
-			var c=i.previousElementSibling;
+			var c=input.previousElementSibling;
 			if(c.tagName=='INPUTCTRL'){
-				ChangePos(c,i);
+				ChangePos(c,input);
 				continue;
 			}
 		}catch(evt){};
 		var elem=document.createElement('InputCtrl');
 		elem.className='InputCtrl';
-		switch(ins[i].type){
+		switch(input.type){
 			case 'text': case 'email':
 				elem.title='点击清除输出的内容！';
 				elem.style.background='url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAYAAACNiR0NAAABPUlEQVR42tXTzStEURjH8dHkXUkxSMqe+A8MQvhTZE+yoWSjlJX8Fyy9ZDeYlfwNSl7m2ikXje9Tv1un69y35Zz61Myce3+dOc/zlEqtuvoy9tvRmTdsGtsYSwlbxjq68oRdoIFDDHqemUUNATbTQidwhh808YkjVLRfxhzutW8+sJEUOoxdvDsvvGJfewu4wZezf4sVXcO/1YZR3V+gF37xghPUnbBQ3+3EHVn32IsdPDsn+XY+hzrpfJG2GVBo4ARF6gorFwm0h5fwGAuzk55ipGhj+woQecNB3lCrVlV/K3Tu7CFWKKv+MYbSwqxaa7iLFeAai55CRX06nhQ4hSvnZE2FV3WnvkI19Jt39rs1n08KrWnM4i21pea3Al1qXBNXv0bJHlxNaForxh7OMZOnMD2YzJiAima/BdcfNBhmqx0AwNkAAAAASUVORK5CYII=)';
 				elem.onclick=function(e){
 					e.target.nextElementSibling.value='';
 				};
-				insertBefore(elem,ins[i]);
+				insertBefore(elem,input);
 				break;
 			case 'password':
 				elem.title='点击显示/隐藏密码！';
@@ -45,7 +46,7 @@ function InputCtrl(){
 					var pwd=e.target.nextElementSibling;
 					pwd.type = (pwd.type=='text') ? 'password' : 'text';
 				};
-				insertBefore(elem,ins[i]);
+				insertBefore(elem,input);
 				break;
 			default:break;
 		}
